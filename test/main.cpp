a61474372d99e838b6dd683bce66208d1df8ca6c
tkchris93 <tannerkchristensen@gmail.com>
2015-10-26 22:12:34 -0600

    This is the real commit. I just got Relation.toStr() working

--- test/main.cpp
+++ test/main.cpp
@@ -1,33 +1,51 @@
 #include <iostream>
 #include <vector>
-#include "my_class.h"
+#include <set>
+#include <string>
 
 using namespace std;
 
-vector<int> my_func(int i)
-{
-    vector<int> list;
-    if (i == 0)
-    {
-        return list;
-    }
-    else
-    {
-        list.push_back(i);
-        for (auto &num : my_func(i-1))
-        {
-            list.push_back(num);
-        }
-    }
-}
-
 int main()
 {
-    vector<int> list = my_func(4);
+    set<vector<string>> table;
+    vector<string> v1;
+    v1.push_back("1");
+    v1.push_back("2");
+    v1.push_back("3");
     
-    for (unsigned int i = 0; i < list.size(); i++)
+    for (unsigned int i = 0; i < v1.size(); i++)
     {
-        cout << list[i] << endl;
+        cout << v1[i] << ",";
+    }
+    cout << endl;
+
+    vector<string> v2;
+    v2.push_back("3");
+    v2.push_back("4");
+    v2.push_back("5");
+    
+    for (unsigned int i = 0; i < v2.size(); i++)
+    {
+        cout << v2[i] << ",";
+    }
+    
+    table.insert(v1);
+    table.insert(v2);
+    
+    cout << endl << endl << table.size() << endl;
+    
+    
+    //accessing vectors in set
+    set<vector<Token>>::iterator it;
+    for (it = table.begin(); it != table.end(); it++)
+    {
+        const vector<string>& vec = (*it);
+        cout << "(";
+        for (unsigned int i = 0; i < vec.size(); i++)
+        {
+            cout << vec[i] << ",";
+        }
+        cout << ")";
     }
     
     return 0;
